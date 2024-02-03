b4443ade105fd8e2ec28cb384c4a9dee7c9f0d2e
Villainelle <61612962+Villainelle@users.noreply.github.com>
2024-02-02 21:55:38 -0800

    Add files via upload

---
+++ Assignments/Assignment 3/website/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function generateLibraryCardNumber() {
+        var cardNumber = Math.floor(1000000000 + Math.random() * 9000000000);
+        localStorage.setItem("libraryCardNumber", cardNumber);
+        // Display the generated card number
+        document.getElementById("libraryCardNumber").textContent = "Your Library Card Number: " + cardNumber;
+    };
+    document.getElementById("generateCardButton").addEventListener("click", generateLibraryCardNumber);
+    var storedCardNumber = localStorage.getItem("libraryCardNumber");
+    if (storedCardNumber) {
+        // Display the stored card number
+        document.getElementById("libraryCardNumber").textContent = "Your Library Card Number: " + storedCardNumber;
+    }
+});
